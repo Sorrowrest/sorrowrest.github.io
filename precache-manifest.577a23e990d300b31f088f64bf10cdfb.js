0fb715a17ce31bc65f8c51f96d48b1757af85ecb
Anastasia <61600483+Sorrowrest@users.noreply.github.com>
2021-02-16 22:33:30 +0300

    Updates

---
+++ precache-manifest.577a23e990d300b31f088f64bf10cdfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54a9f2e277718e08483c82b0038b683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b2bc39ca22c2152740",
+    "url": "/static/css/main.70274651.chunk.css"
+  },
+  {
+    "revision": "eb147f584da0728741e7",
+    "url": "/static/js/2.d51d29d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d51d29d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b2bc39ca22c2152740",
+    "url": "/static/js/main.a5a787a2.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  },
+  {
+    "revision": "18f8277125e082916efea05df3d9f33b",
+    "url": "/static/media/avatar.18f82771.jpg"
+  },
+  {
+    "revision": "d51da72e1f4675ba5aef9c956ed4c562",
+    "url": "/static/media/link.d51da72e.png"
+  }
+]);
